d541eb06ea62f93d031cdfd74e3e18d4253a10c2
Julius Waldmann <julius.waldmann@icloud.com>
2024-10-21 12:00:14 +0200

    updated lazy

--- lua/juliuswaldmann/lazynvim.lua
+++ lua/juliuswaldmann/lazynvim.lua
@@ -36,7 +36,8 @@ return lazy.setup({
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                ensure_installed = {},
+                auto_install = true,
+                ensure_installed = {"c", "pkl"},
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
